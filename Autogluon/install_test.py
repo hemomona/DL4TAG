603e3f2fcd4fc8f9b24eb30f44e92a117bcf4750
hemomona <18146856052@163.com>
2023-07-17 12:11:39 +0800

    change python interpreter from 3.7.9 to 3.10.11 test tabular prediction of Autogluon

---
+++ Autogluon/install_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+# File       : install_test.py
+# Time       ：2023/7/17 11:15
+# Author     ：Jago
+# Email      ：huwl2022@shanghaitech.edu.cn
+# version    ：python 3.10.11
+# Description：
+"""
+# import torch
+# print(torch.cuda.is_available())  # Should be True. Output True
+# print(torch.cuda.device_count())  # Should be > 0.  Output 1
+
+from autogluon.tabular import TabularDataset, TabularPredictor
+
+# a dataset from the cover story of Nature issue 7887: AI-guided intuition for math theorems.
+# The goal is to predict a knot’s signature based on its properties.
+data_url = 'https://raw.githubusercontent.com/mli/ag-docs/main/knot_theory/'
+train_data = TabularDataset(f'{data_url}train.csv')
+print(train_data.head())
+
+# pandas didn’t correctly recognize this data type as categorical, AutoGluon will fix this issue.
+label = 'signature'
+print(train_data[label].describe())
+
+# specifying the label column name and then train on the dataset with TabularPredictor.fit().
+# AutoGluon will recognize this is a multi-class classification task, perform automatic feature engineering,
+# train multiple models, and then ensemble the models to create the final predictor.
+predictor = TabularPredictor(label=label).fit(train_data)
+
+# load test data and predict
+test_data = TabularDataset(f'{data_url}test.csv')
+y_pred = predictor.predict(test_data.drop(columns=[label]))
+print(y_pred.head())
+
+predictor.evaluate(test_data, silent=True)
+
+predictor.leaderboard(test_data, silent=True)
--- ESMFold/protein_folding.py
+++ ESMFold/protein_folding.py
@@ -6,9 +6,18 @@
 # Time       ：2023/7/16 15:09
 # Author     ：Jago
 # Email      ：huwl2022@shanghaitech.edu.cn
-# version    ：python 3.7
+# version    ：python 3.10.11
 # Description：
+    This is a copy of protein_folding.ipynb from
+    https://colab.research.google.com/github/huggingface/notebooks/blob/main/examples/protein_folding.ipynb#scrollTo=949f360e
+
+    It always clashes while preparing model
+
+    I have not solve this problem
 """
+# to set where pytorch saves its pretrained model
+# import os
+# os.environ['TORCH_HOME'] = 'D:/PycharmCache/torchcache'
 
 # to be counted by google (can be skipped)
 from transformers.utils import send_example_telemetry
@@ -19,6 +28,7 @@ send_example_telemetry("protein_folding_notebook", framework="pytorch")
 from transformers import AutoTokenizer, EsmForProteinFolding
 
 tokenizer = AutoTokenizer.from_pretrained("facebook/esmfold_v1")
+# tokenizer = AutoTokenizer.from_pretrained("D:/PycharmCache/torchcache/huggingface/hub/models--facebook--esmfold_v1/snapshots/75a3841ee059df2bf4d56688166c8fb459ddd97a/")
 model = EsmForProteinFolding.from_pretrained("facebook/esmfold_v1", low_cpu_mem_usage=True)
 
 model = model.cuda()
